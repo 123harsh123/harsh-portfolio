6f0d207e3105816894c556f034a4d2583eec5ff0
123harsh123 <87543720+123harsh123@users.noreply.github.com>
2026-09-15 14:31:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Portfolio Loaded Successfully");
+
+
+const links=document.querySelectorAll("nav a");
+
+
+links.forEach(link=>{
+
+link.addEventListener("click",function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+
+});
+
+});
